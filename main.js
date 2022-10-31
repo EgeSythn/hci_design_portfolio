3fd631d7a73c445d3c75f3c2be02bcb809bf541e
EgeSythn <eseyith1@gmail.com>
2022-10-30 23:35:11 -0400

    Assignment 2

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.css'
+var acc = document.getElementsByClassName("accordion");
+var acc2 = document.getElementsByClassName("accordion2");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+  acc2[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
